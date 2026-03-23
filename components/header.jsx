7c205a43223ea33c1a24e28f6264aad822c7f0c7
ishwar gupta <169834413+ishwargupta2004@users.noreply.github.com>
2026-03-23 16:19:01 +0530

    home

--- components/header.jsx
+++ components/header.jsx
@@ -4,10 +4,16 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-import React from 'react'
-import { Show, SignInButton, UserButton } from "@clerk/nextjs";
+import React, { useState } from 'react'
+import { Show, SignInButton, useAuth, UserButton } from "@clerk/nextjs";
+import { BarLoader } from "react-spinners";
+import { Building, Plus, Ticket } from "lucide-react";
 
-const header = () => {
+const Header = () => {
+
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
+  const { isLoaded } = useAuth();
+
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -26,22 +32,73 @@ const header = () => {
 
           {/* Right Side Actions */}
           <div className="flex items-center">
-             <Show when="signed-out">
+
+             <Button
+                variant="ghost"
+                size="sm"
+                onClick={() => setShowUpgradeModal(true)}
+              >
+                Pricing
+              </Button>
+
+            <Button variant="ghost" size="sm" asChild className={"mr-2"}>
+              <Link href="/explore">Explore</Link>
+            </Button>
+
+                          {/* Create Event Button */}
+              <Button size="sm" asChild className="flex gap-2 mr-4">
+                <Link href="/create-event">
+                  <Plus className="w-4 h-4" />
+                  <span className="hidden sm:inline">Create Event</span>
+                </Link>
+              </Button>
+
+            {/* login */}
+            <Show when="signed-out">
+
               <SignInButton mode="modal">
                 <Button size="sm">Sign In</Button>
               </SignInButton>
-              
             </Show>
+
+
             <Show when="signed-in">
-              <UserButton />
+               {/* User Button */}
+              <UserButton
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    avatarBox: "w-9 h-9",
+                  },
+                }}
+              >
+                <UserButton.MenuItems>
+                  <UserButton.Link
+                    label="My Tickets"
+                    labelIcon={<Ticket size={16} />}
+                    href="/my-tickets"
+                  />
+                  <UserButton.Link
+                    label="My Events"
+                    labelIcon={<Building size={16} />}
+                    href="/my-events"
+                  />
+                  <UserButton.Action label="manageAccount" />
+                </UserButton.MenuItems>
+              </UserButton>
             </Show>
           </div>
 
         </div>
-      </nav>
 
+        {!isLoaded && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#a855f7" />
+          </div>
+        )}
+      </nav>
     </>
   )
 }
 
-export default header
+export default Header
